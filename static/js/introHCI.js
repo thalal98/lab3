2730a6eb137325dabe2363f3757f47eaa27c8b25
Thalal Cassim <tmohamed@ucsd.edu>
2019-01-24 10:35:49 -0800

    Jeez

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,24 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
 	});
-
+	$("a.thumbnail").click(projectClick);
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+    	$(description).fadeOut();
+      // description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
+
 }
